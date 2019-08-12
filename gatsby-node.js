19331f5b25bab595e28e55e2882ce7148b5df837
TonyProkop <prokop.tony@yahoo.com>
2019-08-12 07:52:42 -0500

    Fixed build error. Oops.

--- gatsby-node.js
+++ gatsby-node.js
@@ -12,7 +12,6 @@ exports.createPages = async ({ graphql, actions }) => {
           sort: { fields: [frontmatter___date], order: DESC }
           limit: 1000
         ) {
-          group(field: frontmatter)
           edges {
             node {
               fields {
