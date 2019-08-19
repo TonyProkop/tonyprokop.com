c8a969d65afcab5f60693aacc3f52dfbf4bcb25c
TonyProkop <prokop.tony@yahoo.com>
2019-08-19 18:08:02 -0500

    New logo.

--- gatsby-config.js
+++ gatsby-config.js
@@ -66,7 +66,7 @@ module.exports = {
         background_color: `#15121E`,
         theme_color: `#15121E`,
         display: `minimal-ui`,
-        icon: `content/assets/gatsby-icon.png`,
+        icon: `content/assets/logo.png`,
       },
     },
     `gatsby-plugin-offline`,
