ff2b5c2df8cfe0d99fb075b5fbfd176092aa2795
TonyProkop <prokop.tony@yahoo.com>
2019-11-04 06:46:42 -0600

    Styled about page

--- src/pages/about.js
+++ src/pages/about.js
@@ -1,7 +1,7 @@
 import React from "react"
 import { Link } from "gatsby"
+import classnames from "classnames"
 import Layout from "../components/Layout"
-import { ArrowRight } from "../components/Icon"
 import profilePic from "../../content/assets/profile-pic.jpg"
 import SEO from "../components/SEO"
 import { speedwayMotorsUrl, myWeddingUrl } from "../utils/config"
@@ -10,40 +10,40 @@ import styles from "./about.module.scss"
 const About = () => (
   <Layout title="About Me">
     <SEO title="About Me" />
-    <div className={styles.aboutContainer}>
-      <h1>Hi, I'm Tony Prokop.</h1>
-      <Link to="/contact" className={`${styles.getInTouch} callToActionLink`}>
-        Get in touch
-        <ArrowRight />
-      </Link>
+    <div
+      className={classnames(
+        styles.aboutContainer,
+        "section",
+        "contentContainer"
+      )}
+    >
       <img src={profilePic} alt="Tony Prokop" className={styles.profilePic} />
-      <div className="content">
-        <p>
-          I'm a web developer based in Omaha, NE, currently excited about React,
-          static site generators, and Netlify.
-        </p>
-        <p>
-          During the day I help maintain the{" "}
-          <a href={speedwayMotorsUrl} target="_blank" rel="noopener noreferrer">
-            Speedway Motors
-          </a>{" "}
-          ecommerce platform, which supplies street and performance automotive
-          parts to car enthusiasts around the world. At Speedway, I lead a team
-          focused on implementing a website redesign.
-        </p>
-        <p>
-          In my free time you might find me writing for this blog, camping, or
-          helping my beautiful fiancé prepare for{" "}
-          <a href={myWeddingUrl} target="_blank" rel="noopener noreferrer">
-            our wedding
-          </a>{" "}
-          in 2020! 🎉
-        </p>
-        <p>
-          I'm available for side projects,{" "}
-          <Link to="/contact">give me a shout!</Link>
-        </p>
-      </div>
+      <h1>Hi, I'm Tony Prokop.</h1>
+      <p>
+        I'm a front-end web developer based in Omaha, NE, currently excited
+        about React, static site generators, and Netlify.
+      </p>
+      <p>
+        During the day I help maintain the{" "}
+        <a href={speedwayMotorsUrl} target="_blank" rel="noopener noreferrer">
+          Speedway Motors
+        </a>{" "}
+        ecommerce platform, which supplies street and performance automotive
+        parts to car enthusiasts around the world. At Speedway, I lead a team
+        focused on implementing a website redesign.
+      </p>
+      <p>
+        In my free time you might find me playing Zelda: Breath of the Wild,
+        hiking, or helping my beautiful fiancé plan{" "}
+        <a href={myWeddingUrl} target="_blank" rel="noopener noreferrer">
+          our wedding
+        </a>{" "}
+        in 2020! 🎉
+      </p>
+      <p>
+        I'm available for side projects,{" "}
+        <Link to="/contact">give me a shout!</Link>
+      </p>
     </div>
   </Layout>
 )
