c20c7ff5604e7e639561ddc3e8b8526b2b96778d
Tony Prokop <13754823+TonyProkop@users.noreply.github.com>
2020-01-14 12:52:02 -0600

    Styling for the layout

--- eleventy-sample/.eleventy.js
+++ eleventy-sample/.eleventy.js
@@ -1,13 +1,14 @@
-const CleanCSS = require("clean-css");
-const sass = require("./config/sass-process");
+const CleanCSS = require("clean-css")
+const sass = require("./config/sass-process")
 
 module.exports = function(eleventyConfig) {
-  eleventyConfig.addPassthroughCopy("icons");
+  eleventyConfig.addPassthroughCopy("icons")
   eleventyConfig.addFilter("cssmin", function(code) {
-    return new CleanCSS({}).minify(code).styles;
-  });
-  sass("./index.scss", "./_includes/index.css");
+    return new CleanCSS({}).minify(code).styles
+  })
+  sass("./index.scss", "./_includes/index.css")
+  sass("./dark-theme.scss", "./_includes/dark-theme.css")
   return {
-    passthroughFileCopy: true
-  };
-};
+    passthroughFileCopy: true,
+  }
+}
