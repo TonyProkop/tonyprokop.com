a1d0c66f57b412fa2807dd62d443e245bed8a598
Tony Prokop <tonyprokop@Tony-Prokops-MacBook-Pro.local>
2020-01-31 07:48:05 -0600

    Base layout for home page.

--- redesign/.eleventy.js
+++ redesign/.eleventy.js
@@ -1,7 +1,22 @@
+const Terser = require("terser")
+
 module.exports = function(eleventyConfig) {
-  eleventyConfig.addPassthroughCopy("css");
+  eleventyConfig.addPassthroughCopy("css")
+  eleventyConfig.addPassthroughCopy("icons")
+  eleventyConfig.addPassthroughCopy("images")
+  eleventyConfig.addPassthroughCopy("js")
+
+  eleventyConfig.addFilter("jsmin", function(code) {
+    let minified = Terser.minify(code)
+    if (minified.error) {
+      console.log("Terser error: ", minified.error)
+      return code
+    }
+
+    return minified.code
+  })
 
   return {
-    passthroughFileCopy: true
-  };
-};
+    passthroughFileCopy: true,
+  }
+}
