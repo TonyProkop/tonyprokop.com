a26a4cfd4e88454dd9a6fee8c554f56b9bbaeaf7
Tony Prokop <tony.prokop@getflywheel.com>
2020-05-07 07:21:33 -0500

    Started nuxt project.

---
+++ components/Button.vue
@@ -0,0 +1,33 @@
+<template>
+	<button class="button" :type="type">
+		<slot />
+	</button>
+</template>
+
+<script>
+export default {
+	props: {
+		type: {
+			type: String,
+			required: false,
+			default: 'button'
+		}
+	}
+}
+</script>
+
+<style scoped>
+.button {
+	background-color: #5E81AC;
+	color: #D8DEE9;
+	font-size: 18px;
+	display: flex;
+	align-items: center;
+	justify-content: center;
+	padding: 1.5rem 3rem;
+	border-radius: 5rem;
+	border: none;
+	font-weight: bold;
+}
+
+</style>
---
+++ components/Footer.vue
@@ -0,0 +1,72 @@
+<template>
+	<footer>
+		<div class="external-link-container">
+			<a href="mailto:prokop.tony@gmail.com">
+				<EmailSvg />
+			</a>
+			<a href="https://www.linkedin.com/in/tony-prokop/">
+				<LinkedInSvg />
+			</a>
+			<a href="https://github.com/TonyProkop">
+				<GithubSvg />
+			</a>
+		</div>
+		<div class="tagline">© 2020 Tony Prokop Web Development</div>
+	</footer>
+</template>
+
+<script>
+import EmailSvg from '~/static/email.svg?inline';
+import LinkedInSvg from '~/static/linkedin.svg?inline';
+import GithubSvg from '~/static/github.svg?inline';
+
+export default {
+	components: {
+		EmailSvg,
+		LinkedInSvg,
+		GithubSvg,
+	}
+}
+</script>
+
+<style scoped>
+footer {
+	display: flex;
+	flex-direction: column;
+	align-content: center;
+	justify-content: center;
+	text-align: center;
+	padding: 6rem 0;
+}
+
+.tagline {
+	margin-top: 6rem;
+	font-size: 1.4rem;
+}
+
+.external-link-container {
+	display: flex;
+	margin: 0 auto;
+}
+
+.external-link-container a {
+	width: 5rem;
+	height: 5rem;
+	border-radius: 100%;
+	background-color: #3B4252;
+	text-decoration: none;
+	color: #D8DEE9;
+	display: flex;
+	align-items: center;
+	justify-content: center;
+	margin: 0 2rem;
+}
+
+.external-link-container a:hover {
+	background-color: #434C5E;
+}
+
+.external-link-container a:active {
+	background-color: #4C566A;
+}
+</style>
