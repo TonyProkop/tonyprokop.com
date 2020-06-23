8baa0dd58dc6d7d3bf5aacfc5b3c80469f41a156
Tony Prokop <tony.prokop@getflywheel.com>
2020-06-23 08:21:03 -0500

    fixed netlify form

--- components/ContactForm.vue
+++ components/ContactForm.vue
@@ -3,18 +3,18 @@
 		<div class="form-row half">
 			<div class="form-group">
 				<label for="name">Name</label>
-				<input id="name" type="text" />
+				<input id="name" type="text" name="name" />
 			</div>
 			<div class="gutter"></div>
 			<div class="form-group">
 				<label for="email">Email</label>
-				<input id="email" type="text" />
+				<input id="email" type="text" name="email" />
 			</div>
 		</div>
 		<div class="form-row">
 			<div class="form-group">
 				<label for="description">Description of project</label>
-				<textarea id="description" rows="5" />
+				<textarea id="description" rows="5" name="description" />
 			</div>
 		</div>
 		<div class="form-row button-row">
