ca49d0a8e4e2cc123dfe32226699fc293615ed67
Tony Prokop <tony.prokop@getflywheel.com>
2021-04-08 08:13:36 -0500

    Added GA

--- nuxt.config.js
+++ nuxt.config.js
@@ -1,54 +1,58 @@
 
 export default {
-  target: 'static',
-  mode: 'universal',
-  /*
-  ** Headers of the page
-  */
-  head: {
-    title: process.env.npm_package_name || '',
-    meta: [
-      { charset: 'utf-8' },
-      { name: 'viewport', content: 'width=device-width, initial-scale=1' },
-      { hid: 'description', name: 'description', content: process.env.npm_package_description || '' }
-    ],
-    link: [
-      { rel: 'icon', type: 'image/x-icon', href: '/favicon.svg' }
-    ]
-  },
-  /*
-  ** Customize the progress-bar color
-  */
-  loading: { color: '#fff' },
-  /*
-  ** Global CSS
-  */
-  css: [
-  ],
-  /*
-  ** Plugins to load before mounting the App
-  */
-  plugins: [
-  ],
-  /*
-  ** Nuxt.js dev-modules
-  */
-  buildModules: [
-  ],
-  /*
-  ** Nuxt.js modules
-  */
-  modules: [
-    '@nuxtjs/svg',
-  ],
-  /*
-  ** Build configuration
-  */
-  build: {
-    /*
-    ** You can extend webpack config here
-    */
-    extend (config, ctx) {
-    }
-  }
+	target: 'static',
+	mode: 'universal',
+	/*
+	** Headers of the page
+	*/
+	head: {
+		title: process.env.npm_package_name || '',
+		meta: [
+			{ charset: 'utf-8' },
+			{ name: 'viewport', content: 'width=device-width, initial-scale=1' },
+			{ hid: 'description', name: 'description', content: process.env.npm_package_description || '' }
+		],
+		link: [
+			{ rel: 'icon', type: 'image/x-icon', href: '/favicon.svg' }
+		]
+	},
+	/*
+	** Customize the progress-bar color
+	*/
+	loading: { color: '#fff' },
+	/*
+	** Global CSS
+	*/
+	css: [
+	],
+	/*
+	** Plugins to load before mounting the App
+	*/
+	plugins: [
+	],
+	/*
+	** Nuxt.js dev-modules
+	*/
+	buildModules: [
+		'@nuxtjs/google-analytics'
+	],
+	/*
+	** Nuxt.js modules
+	*/
+	modules: [
+		'@nuxtjs/svg',
+	],
+	/*
+	** Build configuration
+	*/
+	build: {
+		/*
+		** You can extend webpack config here
+		*/
+		extend (config, ctx) {
+		}
+	},
+	googleAnalytics: {
+		id: 'G-Y0EKH5J6S5'
+	}
 }
