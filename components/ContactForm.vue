ed2634e21cb3ce372f58b4643f471af70285dbdc
Tony Prokop <tony.prokop@getflywheel.com>
2021-07-20 19:09:50 -0500

    trying something

--- components/ContactForm.vue
+++ components/ContactForm.vue
@@ -19,7 +19,7 @@
 			</div>
 		</div>
 		<div class="form-row button-row">
-			<Button type="submit" text="Send message" :primary="true" />
+			<button class="button" type="submit" text="Send message" :primary="true" />
 		</div>
 	</form>
 </template>
