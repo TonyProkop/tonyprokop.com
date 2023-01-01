a7b2fe2502daa799175dddcfe06fa1846bf5ccfb
Tony Prokop <tony.prokop@wpengine.com>
2023-01-01 15:13:55 -0600

    Blog

---
+++ layouts/blog.vue
@@ -0,0 +1,63 @@
+<template>
+	<div class="page-container --small">
+		<Header />
+		<main>
+			<nuxt />
+		</main>
+		<Footer />
+	</div>
+</template>
+
+<script>
+import Header from "../components/Header"
+import Footer from "../components/Footer"
+
+export default {
+	components: {
+		Header,
+		Footer,
+	},
+}
+</script>
+
+<style lang="scss">
+@import "~assets/base.scss";
+
+.page-container.--small {
+	background-color: var(--clr-white);
+	color: var(--clr-black);
+
+	*:not(svg):not(path) {
+		color: var(--clr-black);
+	}
+
+	h1 {
+		font-size: 3.6rem;
+	}
+
+	h2 {
+		font-size: 2.4rem;
+	}
+
+	h3 {
+		font-size: 1.8rem;
+	}
+
+	h1,
+	h2,
+	h3,
+	h4,
+	h5,
+	h6 {
+		margin-bottom: 3rem;
+	}
+
+	p {
+		margin-bottom: 2rem;
+	}
+
+	.section-content {
+		--max-width: 600px;
+	}
+}
+</style>
---
+++ pages/blog/seven-reasons-every-small-or-local-business-needs-a-website.vue
@@ -0,0 +1,82 @@
+<template>
+	<section>
+		<div class="section-content">
+			<h1>7 Reasons Every Small or Local Business Needs a Website</h1>
+			<p>
+				As a small business owner, having an online presence is essential to
+				staying competitive in today's digital world. A website is a great way
+				to showcase your products and services to potential customers, as well
+				as providing valuable information about your business. Here are five
+				reasons why every local business needs a website:
+			</p>
+
+			<h2>Increased Visibility</h2>
+			<p>
+				A website is the perfect platform to showcase your products and services
+				to potential customers. With a well-designed website, you can reach a
+				wider audience and increase your visibility in the marketplace.
+			</p>
+
+			<h2>Improved Brand Recognition</h2>
+			<p>
+				Having a website allows you to establish a strong online presence and
+				build your brand. You can create a unique design and use it to showcase
+				your products and services, as well as providing valuable information to
+				potential customers.
+			</p>
+
+			<h2>Improved Brand Awareness</h2>
+			<p>
+				Having a website is an excellent way to build brand awareness and
+				establish your business as a legitimate, reputable organization. Your
+				website can be used to display your logo, mission statement, and other
+				important information about your business.
+			</p>
+
+			<h2>Increased Customer Engagement</h2>
+			<p>
+				With a website, you can engage with customers in a more meaningful way
+				and build relationships. You can use it to provide customer support,
+				answer frequently asked questions, offer special promotions and
+				discounts, or even provide a platform for customers to leave reviews and
+				feedback.
+			</p>
+
+			<h2>Improved SEO Rankings</h2>
+			<p>
+				Having a website can help to boost your SEO rankings, which means more
+				potential customers will be able to find your business online. You can
+				also use SEO tactics such as keyword research and content optimization
+				to further improve your rankings.
+			</p>
+
+			<h2>Cost-Effective Marketing</h2>
+			<p>
+				Having a website is a cost-effective way to market your business. You
+				can use your website to create content, such as blog posts and videos,
+				to reach a wider audience. You can also use your website to advertise
+				your products and services, as well as connect with customers on social
+				media.
+			</p>
+
+			<h2>Increased Sales</h2>
+			<p>
+				A website can be used to generate sales and revenue for your business.
+				With an e-commerce website, you can easily accept payments and sell your
+				products and services online.
+			</p>
+
+			<p>
+				Overall, having a website is an essential part of any small or local
+				business's success. If you don't have a website, now is the time to get
+				one and take your business to the next level.
+			</p>
+		</div>
+	</section>
+</template>
+
+<script>
+export default {
+	layout: "blog",
+}
+</script>
