8dd1e430c86bd7c79490e68e0fb8ae6324aa3323
Tony Prokop <tony.prokop@wpengine.com>
2024-10-03 09:34:12 -0500

    more

--- src/app/components/FloodFill.tsx
+++ src/app/components/FloodFill.tsx
@@ -1,4 +1,6 @@
 import { Box, Grid, Slider } from '@mui/material'
+import { Prism as SyntaxHighlighter } from 'react-syntax-highlighter'
+import rosePinePrism from '@/rose-pine-prism'
 
 const FloodFill = () => {
   const grid = [
@@ -15,8 +17,26 @@ const FloodFill = () => {
     [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
     [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
   ]
+  const code = `
+const TEST = 'test';
+var countSeniors = function(details) {
+  const test = parseInt(details[i].substring(11, 13))
+  let count = 0;
+
+  for (let i = 0; i < details.length; i++) {
+      const age = parseInt(details[i].substring(11, 13))
+      if (age > 60) count++
+  }
+
+  return count;
+};`
+
+
   return (
     <Box>
+      <SyntaxHighlighter language="javascript" style={rosePinePrism}>
+        {code}
+      </SyntaxHighlighter>
       <Grid container spacing={2} sx={{
         border: '1px solid var(--clr-overlay)',
         marginBlock: 10,
