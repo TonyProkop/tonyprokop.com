a2908314a385811cf0534d1952c990184529eb90
Tony Prokop <tony.prokop@wpengine.com>
2024-10-25 15:38:42 -0500

    more

--- src/app/components/NewQuotes.tsx
+++ src/app/components/NewQuotes.tsx
@@ -17,8 +17,7 @@ const quotes = [
     name: "Cathal Mac Donnacha",
     position: "Principal Software Engineer",
     text: [
-      "Tony has had a massive impact on User Portal and his fellow devs. He shows great initiative, consistently goes above and beyond expectations while making sure to collaborate where everyone feels valued and motivated.",
-      "Tony's contributions have significantly elevated the quality of features and project wide initiatives, being a constant inspiration for others. We are incredibly fortunate to have him and his expertise.",
+      "Tony has had a massive impact on User Portal and his fellow devs. He shows great initiative, consistently goes above and beyond expectations, and is constant inspiration for others. We are incredibly fortunate to have him and his expertise.",
     ],
     image: Cathal,
   },
@@ -26,7 +25,7 @@ const quotes = [
     name: "Greg Schmaderer",
     position: "Bussiness Owner",
     text: [
-      "I worked with Tony to setup a website for my small business. Tony was consultative and responsive throughout the entire process. This enabled us to get the website built quickly. I would not hesitate to recommend Tony. He is easy to work with.",
+      "I worked with Tony to setup a website for my small business. Tony was consultative and responsive throughout the entire process. This enabled us to get the website built quickly. I wouldn't hesitate to recommend Tony. He is easy to work with.",
     ],
     image: Greg,
   },
@@ -34,7 +33,7 @@ const quotes = [
     name: "Shannon Sorensen",
     position: "Senior Product Designer",
     text: [
-      "Forget the AI hype, all focus should be in cloning Tony. Dude is the best parts of humans and machine. He cranks out perfect code with a thoughtfulness and humor.",
+      "Forget the AI hype, all focus should be in cloning Tony. The dude is the best parts of humans and machine. He cranks out perfect code with a thoughtfulness and humor.",
     ],
     image: Shannon,
   },
@@ -42,8 +41,7 @@ const quotes = [
     name: "Vivek Patel",
     position: "Senior Software Engineer",
     text: [
-      'This recognition is long overdue, Tony has helped me out on several occasions now - he has become my "go-to" person when I have front-end or growth suite questions.',
-      "The best part about Tony is that he always makes my issue feel important and finds time to help out! Appreciate you Tony!",
+      'This recognition is long overdue, Tony has helped me out on several occasions now - he has become my "go-to" person when I have frontend questions. He always makes my issue feel important and finds time to help out! Appreciate you Tony!',
     ],
     image: Vivek,
   },
@@ -51,7 +49,7 @@ const quotes = [
     name: "Savannah Albanez",
     position: "Software Engineer",
     text: [
-      "I not sure how he does it, but Tony has been everywhere all at once the last few weeks. With the Accessibility release and the Dashboard release he still has time to hop on a zoom, answer questions async, and notes revisions needed on past PRs - All while breezing through sprint work. Truly an inspiration friend!",
+      "I not sure how he does it, but Tony has been everywhere the last few weeks. He somehow finds time to hop on a zoom, answer questions async, and give feedback on PRs all while breezing through his own sprint work. Truly an inspiration friend!",
     ],
     image: Savannah,
   },
@@ -85,23 +83,28 @@ const NewQuotes = () => {
   const positions = ["left1", "left", "center", "right", "right1"];
 
   useEffect(() => {
-    const setCarousePosition = () => {
+    const setupCarousel = () => {
       if (containerRef?.current?.offsetWidth !== undefined) {
         setClientWidth(document.documentElement.clientWidth)
         setCenter(containerRef.current.offsetWidth / 2)
       }
     }
 
-    setCarousePosition()
+    setupCarousel()
 
     window.addEventListener('resize', () => {
-      setCarousePosition()
+      setupCarousel()
     })
+
+    const interval = setInterval(() => {
+      // handleNext()
+    }, 7000)
 
     return () => {
       window.removeEventListener('resize', () => {
-        setCarousePosition()
+        setupCarousel()
       })
+      clearInterval(interval)
     }
   }, [])
 
@@ -115,28 +118,28 @@ const NewQuotes = () => {
 
   return (
     <Container maxWidth="lg">
-      <Stack spacing={1}>
-        <Typography variant="h2Alt">What others are saying</Typography>
-        <IconButton onClick={handleBack}>
-          <ArrowLeft />
-        </IconButton>
-        <IconButton onClick={handleNext}>
-          <ArrowRight />
-        </IconButton>
-        <Box sx={{ display: 'grid', justifyContent: 'center' }}>
-          <Box ref={containerRef} sx={{ display: 'grid', position: 'relative', overflow: 'hidden', width: `${clientWidth}px` }}>
-            {quotes.map((quote, index) =>
-              <motion.div
-                key={quote.name}
-                animate={positions[positionIndexes[index]]}
-                transition={{ duration: .5 }}
-                style={{ gridRow: '1', gridColumn: '1', maxWidth: '500px', width: '100%' }}
-                variants={imageVariants}
-              >
-                <Quote {...quote} />
-              </motion.div>
-            )}
-          </Box>
+      <Stack spacing={4} alignItems="center">
+        <Typography variant="h2Alt" alignSelf="flex-start">What others are saying</Typography>
+        <Box ref={containerRef} sx={{ display: 'grid', position: 'relative', overflow: 'hidden', width: `${clientWidth}px` }}>
+          {quotes.map((quote, index) =>
+            <motion.div
+              key={quote.name}
+              animate={positions[positionIndexes[index]]}
+              transition={{ duration: .5 }}
+              style={{ gridRow: '1', gridColumn: '1', maxWidth: '500px', width: '100%' }}
+              variants={imageVariants}
+            >
+              <Quote {...quote} />
+            </motion.div>
+          )}
+        </Box>
+        <Box>
+          <IconButton onClick={handleBack}>
+            <ArrowLeft />
+          </IconButton>
+          <IconButton onClick={handleNext}>
+            <ArrowRight />
+          </IconButton>
         </Box>
       </Stack>
     </Container>
